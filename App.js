67f59fd97ee09c9bd2c50dfc4f0397513a4ab424
Zen99 <roshaaanrulz@gmail.com>
2023-04-01 16:42:16 +0530

    Landing page created

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import LandingPage from './src/screens/LandingPage';
 
 export default function App() {
   return (
+    // <View><Text>Hi</Text></View>
     <LandingPage/>
   );
 }
--- src/globals/styles.js
+++ src/globals/styles.js
@@ -1,7 +1,55 @@
-import  {getStatusBarHeight}  from 'react-native-status-bar-height';
-import { Dimensions } from 'react-native';
+import { StatusBar,Dimensions,StyleSheet } from 'react-native';
+const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : StatusBar.currentHeight;
+
+import {
+    Poppins_100Thin,
+    Poppins_100Thin_Italic,
+    Poppins_200ExtraLight,
+    Poppins_200ExtraLight_Italic,
+    Poppins_300Light,
+    Poppins_300Light_Italic,
+    Poppins_400Regular,
+    Poppins_400Regular_Italic,
+    Poppins_500Medium,
+    Poppins_500Medium_Italic,
+    Poppins_600SemiBold,
+    Poppins_600SemiBold_Italic,
+    Poppins_700Bold,
+    Poppins_700Bold_Italic,
+    Poppins_800ExtraBold,
+    Poppins_800ExtraBold_Italic,
+    Poppins_900Black,
+    Poppins_900Black_Italic,
+  } from '@expo-google-fonts/poppins';
+
+//   export default () => {
+//     let [fontsLoaded] = useFonts({
+//       Poppins_100Thin,
+//       Poppins_100Thin_Italic,
+//       Poppins_200ExtraLight,
+//       Poppins_200ExtraLight_Italic,
+//       Poppins_300Light,
+//       Poppins_300Light_Italic,
+//       Poppins_400Regular,
+//       Poppins_400Regular_Italic,
+//       Poppins_500Medium,
+//       Poppins_500Medium_Italic,
+//       Poppins_600SemiBold,
+//       Poppins_600SemiBold_Italic,
+//       Poppins_700Bold,
+//       Poppins_700Bold_Italic,
+//       Poppins_800ExtraBold,
+//       Poppins_800ExtraBold_Italic,
+//       Poppins_900Black,
+//       Poppins_900Black_Italic,
+//     });
+// }
+// export const fonts={
+//     bold:
+// }
+
 export const colors={
-    orange:'#FF8C01',
+    ThemeBlack:'#111111',
 
     grey:'#28231F',
     grey2:'#748c94',
@@ -11,7 +59,7 @@ export const colors={
 }
 
 export const parameters={
-    statusBarHeight :getStatusBarHeight(),
+    statusBarHeight :STATUSBAR_HEIGHT,
     headerHeight:90,
     SCREEN_WIDTH:Dimensions.get('window').width,
     SCREEN_HEIGHT:Dimensions.get('window').height
@@ -19,6 +67,48 @@ export const parameters={
 export const errors={
     fontColor:'red'
 }
+export const globalStyles = StyleSheet.create({
+    button1:{
+        height:50,
+        width:350,
+        backgroundColor:colors.ThemeBlack,
+        borderRadius:50,
+        alignItems:"center",
+        justifyContent:"center",
+        // marginTop:50
+    },
+    button1Text:{
+        color:colors.white,
+        fontSize:22,
+        fontFamily:'Roboto',
+        marginTop:-2
+    },
+    alreadyContainer:{
+        display:"flex",
+        flexDirection:"row",
+        justifyContent:"center",
+        alignItems:"center",
+        marginTop:20
+    },
+    button2:{
+        alignSelf:'center',
+        alignItems:"center",
+        justifyContent:"center",
+    },
+    button2Text:{
+        color:colors.font,
+        fontSize:20,
+        fontWeight:'bold',
+        textDecorationLine:"underline",
+        fontFamily:'Roboto',
+        marginTop:-2
+    },
+    smallText:{
+        fontSize:20,
+        alignSelf:'center',
+        marginRight:10
+    },
+})
 
 // export const mapStyle=[
 //   {
